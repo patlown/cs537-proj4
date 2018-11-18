d9696c7f040e472fe5f8a944589b55412a169ca1
Pat Lown <lown@wisc.edu>
2018-11-18 15:19:09 -0600

    made range tree structs and created test file

--- range_tree.h
+++ range_tree.h
@@ -2,7 +2,23 @@
 #define RANGE_TREE_H
 
 
+/*
+This structure will hold intervals of address ranges.
+The low will represent the lower addr and high will represent higher addr
+This will be used with as a data field for interval tree structure nodes.
+*/
+typedef struct interval
+{
+    size_t low;
+    size_t high;
+} interval;
 
-
+typedef struct tree_node
+{
+    interval *i;
+    size_t max;
+    tree_node *left;
+    tree_node *right;
+} tree_node;
 
 #endif RANGE_TREE_H
---
+++ tests.c
@@ -0,0 +1,11 @@
+
+
+int main(){
+
+    size_t int_size = sizeof(int);
+    int* x = malloc(sizeof(int));
+
+    return 1;
+
+    
+}
