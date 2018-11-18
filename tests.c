e3fee934a8948389d8cb0511949a6b0f64fa07f2
Patrick Lown <lown@royal-05.cs.wisc.edu>
2018-11-18 15:53:51 -0600

    tested addr arithmetic

--- tests.c
+++ tests.c
@@ -1,10 +1,21 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(){
 
     size_t int_size = sizeof(int);
-    int* x = malloc(sizeof(int));
+    void* pointer = malloc(sizeof(int));
 
+    printf("Integers are of size:%zu\n",int_size);	    
+    printf("This integers address is:%p\n",(void*)pointer);
+    
+    void* pointer2 = ((char*)pointer) + int_size;
+    
+    printf("After adding the size of int to x, x is now pointing to:%p\n",(void*)pointer2);
+	
+    if(pointer2 > pointer){
+	printf("works");
+    }    
     return 1;
 
     
